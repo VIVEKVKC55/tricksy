20a811c1476c27c2cf181fc8f6092164b15e93d1
Vivek Kumar <vivekvkc55@gmail.com>
2025-09-26 20:49:32 +0530

    STATIC_URL = '/static/' STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
    MEDIA_URL = '/media/'
    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

--- tricksy/settings.py
+++ tricksy/settings.py
@@ -127,13 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-if os.getenv('ENVIRONMENT') == 'PROD':
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-else:
-    STATICFILES_DIRS = (
-        os.path.normpath(os.path.join(BASE_DIR, "static")),
-    )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
