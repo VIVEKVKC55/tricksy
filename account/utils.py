115c81159382abb3f02ba46140363c9edd5ce23d
Vivek Kumar <vivekvkc55@gmail.com>
2025-09-26 23:03:09 +0530

    fixed permitions

--- account/utils.py
+++ account/utils.py
@@ -1,4 +1,6 @@
 # utils.py
+from django.shortcuts import redirect
+from django.urls import reverse
 from tricksy.constants import PERMISSIONS
 from account.models import Role, RolePermissions
 
@@ -8,6 +10,10 @@ def user_has_access(user, permission_code):
        - Superadmin → always True
        - Subadmin → check RolePermissions for 'subadmin'
     """
+
+    # Step 1: If user not logged in → redirect to login page
+    if not user.is_authenticated:
+        return redirect(reverse('account:login'))  # make sure you have 'login' named URL
     # if user doesn't have a role
     if not hasattr(user, "role"):
         return False
